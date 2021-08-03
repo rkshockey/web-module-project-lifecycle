04ec3f115aaee5b803f39ec6cd2fbc3161aa6337
rkshockey <rkshockey@gmail.com>
2021-08-03 16:18:41 -0400

    User card is functionally implemented

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,16 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
 import UserCard from './components/UserCard';
 import FollowerList from './components/FollowerList';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <p>App</p>
-      <UserCard />
-      <FollowerList />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    userObj: {},
+    followerArr: [],
+    followingArr: [],
+    formValue: 'rkshockey'
+}
+
+  componentDidMount(){
+    axios.get('https://api.github.com/users/rkshockey')
+      .then(res => {
+        this.setState({
+          ...this.state,
+          userObj: res.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if (prevState.userObj !== this.state.userObj){
+      axios.get(this.state.userObj.followers_url)
+        .then(res => {
+          this.setState({
+            ...this.state,
+            followerArr: res.data
+          })
+        })
+        .catch(err => console.log(err));
+      axios.get(`${this.state.userObj.url}/following`)
+        .then(res => this.setState({
+          ...this.state,
+          followingArr: res.data
+        }))
+        .catch(err => console.log(err))
+    }
+  }
+
+  handleChangeUser = (url) => {
+    axios.get(url)
+    .then(res => {
+      this.setState({
+        ...this.state,
+        userObj: res.data
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
+  handleChangeForm = (value) => {
+    this.setState({
+      ...this.state,
+      formBalue: value
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Github User Portfolio</h1>
+        <UserCard
+          userObj={this.state.userObj}
+          followingArr={this.state.followingArr} 
+          formValue={this.state.formValue}
+          handleChangeUser={this.handleChangeUser}
+          handleChangeForm={this.handleChangeForm} />
+        <FollowerList followerArr={this.state.followerArr} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,7 +1,31 @@
 import React from 'react'
 
 function UserCard (props){
-    return <p>UserCard</p>
+    const { userObj, followingArr, formValue, handleChangeUser, handleChangeForm } = props
+
+    function handleChange (e){
+        handleChangeForm(e.target.value);
+        handleChangeUser(e.target.value);
+    }
+
+    return (
+        <div className='user-card'>
+            <img src={userObj.avatar_url} alt={userObj.name} />
+                <div className='user-details'>
+                <h3>{userObj.name}</h3>
+                <p>{userObj.company ? userObj.company : 'No company'}, {userObj.location}</p>
+                <a href={userObj.html_url}>{userObj.login}</a>
+                <p>Hireable: {userObj.hireable ? 'Yes' : 'No'}</p>
+                <form>
+                    <select value={formValue} onChange={handleChange}>
+                        <option value={userObj.url}>--Following:--</option>
+                        {followingArr.map(item => <option value={item.url} key={item.id}>{item.login}</option>)}
+                    </select>
+                </form>
+                <p>{userObj.bio}</p>
+            </div>
+        </div>
+    )
 }
 
 export default UserCard
