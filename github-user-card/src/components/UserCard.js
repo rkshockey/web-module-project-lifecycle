1a92785db2d1a550bdabce0c77970a8b7ee2d030
rkshockey <rkshockey@gmail.com>
2021-08-03 19:00:49 -0400

    Implemented Github Calendar

--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import GitHubCalendar from 'github-calendar'
 
 function UserCard (props){
     const { userObj, followingArr, formValue, handleChangeUser, handleChangeForm } = props
@@ -8,21 +9,28 @@ function UserCard (props){
         handleChangeUser(e.target.value);
     }
 
+    GitHubCalendar('.calendar', userObj.login)
+
     return (
         <div className='user-card'>
-            <img src={userObj.avatar_url} alt={userObj.name} />
-                <div className='user-details'>
-                <h3>{userObj.name ? userObj.name : userObj.login}</h3>
-                <p>{userObj.company ? userObj.company : 'No company'}, {userObj.location}</p>
-                <a href={userObj.html_url}>{userObj.login}</a>
-                <p>Hireable: {userObj.hireable ? 'Yes' : 'No'}</p>
-                <form>
-                    <select value={formValue} onChange={handleChange}>
-                        <option value={userObj.url}>--Following:--</option>
-                        {followingArr.map(item => <option value={item.url} key={item.id}>{item.login}</option>)}
-                    </select>
-                </form>
-                <p>{userObj.bio}</p>
+            <div className='user-data'>
+                <img src={userObj.avatar_url} alt={userObj.name} />
+                    <div className='user-details'>
+                    <h3>{userObj.name ? userObj.name : userObj.login}</h3>
+                    <p>{userObj.company ? userObj.company : 'No company'}, {userObj.location}</p>
+                    <a href={userObj.html_url}>{userObj.login}</a>
+                    <p>Hireable: {userObj.hireable ? 'Yes' : 'No'}</p>
+                    <form>
+                        <select value={formValue} onChange={handleChange}>
+                            <option value={userObj.url}>--Following:--</option>
+                            {followingArr.map(item => <option value={item.url} key={item.id}>{item.login}</option>)}
+                        </select>
+                    </form>
+                    <p>{userObj.bio}</p>
+                </div>
+            </div>
+            <div class="calendar">
+                Loading the data just for you.
             </div>
         </div>
     )
